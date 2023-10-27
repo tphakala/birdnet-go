074ef1845939a6bffd41e2e8c0f16023236c99a0
Tomi P. Hakala <tomi.hakala@pobox.fi>
2023-10-27 14:20:58 +0000

    % added realtime capture and processing

---
+++ pkg/myaudio/process.go
@@ -0,0 +1,63 @@
+package myaudio
+
+import "errors"
+
+// ConvertToFloat32 converts a byte slice representing sample to a 2D slice of float32 samples.
+// The function supports 16, 24, and 32 bit depths.
+func ConvertToFloat32(sample []byte, bitDepth int) ([][]float32, error) {
+	switch bitDepth {
+	case 16:
+		return [][]float32{convert16BitToFloat32(sample)}, nil
+	case 24:
+		return [][]float32{convert24BitToFloat32(sample)}, nil
+	case 32:
+		return [][]float32{convert32BitToFloat32(sample)}, nil
+	default:
+		return nil, errors.New("unsupported audio bit depth")
+	}
+}
+
+// convert16BitToFloat32 converts 16-bit sample to float32 values.
+func convert16BitToFloat32(sample []byte) []float32 {
+	length := len(sample) / 2
+	float32Data := make([]float32, length)
+	divisor := float32(32768.0)
+
+	for i := 0; i < length; i++ {
+		sample := int16(sample[i*2]) | int16(sample[i*2+1])<<8
+		float32Data[i] = float32(sample) / divisor
+	}
+
+	return float32Data
+}
+
+// convert24BitToFloat32 converts 24-bit sample to float32 values.
+func convert24BitToFloat32(sample []byte) []float32 {
+	length := len(sample) / 3
+	float32Data := make([]float32, length)
+	divisor := float32(8388608.0)
+
+	for i := 0; i < length; i++ {
+		sample := int32(sample[i*3]) | int32(sample[i*3+1])<<8 | int32(sample[i*3+2])<<16
+		if (sample & 0x00800000) > 0 {
+			sample |= ^0x00FFFFFF // Two's complement sign extension
+		}
+		float32Data[i] = float32(sample) / divisor
+	}
+
+	return float32Data
+}
+
+// convert32BitToFloat32 converts 32-bit sample to float32 values.
+func convert32BitToFloat32(sample []byte) []float32 {
+	length := len(sample) / 4
+	float32Data := make([]float32, length)
+	divisor := float32(2147483648.0)
+
+	for i := 0; i < length; i++ {
+		sample := int32(sample[i*4]) | int32(sample[i*4+1])<<8 | int32(sample[i*4+2])<<16 | int32(sample[i*4+3])<<24
+		float32Data[i] = float32(sample) / divisor
+	}
+
+	return float32Data
+}
---
+++ pkg/myaudio/ringbuffer.go
@@ -0,0 +1,87 @@
+package myaudio
+
+import (
+	"fmt"
+	"log"
+	"sync"
+	"time"
+
+	"github.com/smallnest/ringbuffer"
+	"github.com/tphakala/go-birdnet/pkg/birdnet"
+)
+
+const (
+	chunkSize      = 144000
+	pollingTimeout = time.Millisecond * 100
+)
+
+var (
+	ringBuffer  *ringbuffer.RingBuffer
+	bufferMutex sync.Mutex
+	quitChannel = make(chan struct{})
+)
+
+func writeToBuffer(data []byte) {
+	bufferMutex.Lock()
+	defer bufferMutex.Unlock()
+	ringBuffer.Write(data)
+}
+
+func readFromBuffer() []byte {
+	bufferMutex.Lock()
+	defer bufferMutex.Unlock()
+
+	bytesWritten := ringBuffer.Length() - ringBuffer.Free()
+	if bytesWritten < chunkSize {
+		//fmt.Println("Not enough data in buffer")
+		return nil
+	}
+
+	data := make([]byte, chunkSize)
+	_, err := ringBuffer.Read(data)
+	if err != nil {
+		return nil
+	}
+	return data
+}
+
+func BufferMonitor() {
+	for {
+		select {
+		case <-quitChannel:
+			return
+		default:
+			data := readFromBuffer()
+			fmt.Println("data length: ", len(data))
+			if data != nil {
+				processData(data)
+			} else {
+				time.Sleep(pollingTimeout)
+			}
+		}
+	}
+}
+
+func processData(data []byte) {
+	//log.Printf("Processing data: %b\n", data)
+	// get time stamp to calculate processing time
+	ts := time.Now()
+
+	// temporary assignments
+	var bitDepth int = 16
+	var sensitivity float64 = 1.25
+
+	sampleData, err := ConvertToFloat32(data, bitDepth)
+	if err != nil {
+		log.Fatalf("Error converting to float32: %v", err)
+	}
+	results, err := birdnet.Predict(sampleData, sensitivity)
+	if err != nil {
+		log.Fatalf("Error predicting: %v", err)
+	}
+	te := time.Now()
+	fmt.Printf("processing time %v", te.Sub(ts))
+
+	fmt.Println(results)
+	//fmt.Println("Processed audio data of length:", len(data))
+}
