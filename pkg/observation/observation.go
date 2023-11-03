47750d066ea3068319726960d6e93e472df81334
Tomi P. Hakala <tomi.hakala@pobox.fi>
2023-11-04 00:54:46 +0200

    cleanups & added comments

--- pkg/observation/observation.go
+++ pkg/observation/observation.go
@@ -43,107 +43,136 @@ func ParseSpeciesString(species string) (string, string, string) {
 	return species, species, ""
 }
 
-// New creates a new Observation.
+// New creates and returns a new Note with the provided parameters and current date and time.
+// It uses the configuration and parsing functions to set the appropriate fields.
 func New(cfg *config.Settings, beginTime, endTime float64, species string, confidence float64, latitude, longitude float64, clipName string, elapsedTime time.Duration) Note {
+	// Parse the species string to get the scientific name, common name, and species code.
 	scientificName, commonName, speciesCode := ParseSpeciesString(species)
 
+	// Return a new Note struct populated with the provided parameters as well as the current date and time.
 	return Note{
-		Date:           time.Now().Format("2006-01-02"), // Using the current date
-		Time:           time.Now().Format("15:04:05"),   // Using the current time (24-hour format
-		InputFile:      cfg.InputFile,
-		BeginTime:      beginTime,
-		EndTime:        endTime,
-		SpeciesCode:    speciesCode,
-		ScientificName: scientificName,
-		CommonName:     commonName,
-		Confidence:     confidence,
-		Latitude:       latitude,
-		Longitude:      longitude,
-		Threshold:      cfg.Threshold,
-		Sensitivity:    cfg.Sensitivity,
-		ClipName:       clipName,
-		ProcessingTime: elapsedTime,
+		Date:           time.Now().Format("2006-01-02"), // Use ISO 8601 date format.
+		Time:           time.Now().Format("15:04:05"),   // Use 24-hour time format.
+		InputFile:      cfg.InputFile,                   // From the provided configuration settings.
+		BeginTime:      beginTime,                       // Start time of the observation.
+		EndTime:        endTime,                         // End time of the observation.
+		SpeciesCode:    speciesCode,                     // Parsed species code.
+		ScientificName: scientificName,                  // Parsed scientific name of the species.
+		CommonName:     commonName,                      // Parsed common name of the species.
+		Confidence:     confidence,                      // Confidence score of the observation.
+		Latitude:       latitude,                        // Geographic latitude where the observation was made.
+		Longitude:      longitude,                       // Geographic longitude where the observation was made.
+		Threshold:      cfg.Threshold,                   // Threshold setting from configuration.
+		Sensitivity:    cfg.Sensitivity,                 // Sensitivity setting from configuration.
+		ClipName:       clipName,                        // Name of the audio clip.
+		ProcessingTime: elapsedTime,                     // Time taken to process the observation.
 	}
 }
 
-// LogNote is the central function for logging observations.
+// LogNote is the central function for logging observations. It writes a note to a log file and/or database
+// depending on the provided configuration settings.
 func LogNote(cfg *config.Settings, note Note) error {
+	// If a log file path is specified in the configuration, attempt to log the note to this file.
 	if cfg.LogFile != "" {
-		// Save to Log File
 		if err := LogNoteToFile(cfg, note); err != nil {
-			return fmt.Errorf("failed to log note to file: %v", err)
+			// If an error occurs when logging to a file, wrap and return the error.
+			return fmt.Errorf("failed to log note to file: %w", err)
 		}
 	}
 
+	// If the configuration specifies a database (and it's not set to "none"), attempt to save the note to the database.
 	if cfg.Database != "none" {
-		// Save to Database
 		if err := SaveToDatabase(note); err != nil {
-			return fmt.Errorf("failed to save note to database: %v", err)
+			// If an error occurs when saving to the database, wrap and return the error.
+			return fmt.Errorf("failed to save note to database: %w", err)
 		}
 	}
 
+	// Return nil to indicate that the logging operations completed without error.
 	return nil
 }
 
-// WriteNotes writes the slice of notes to the specified destination.
+// WriteNotesTable writes a slice of Note structs to a table-formatted text output.
 // The output can be directed to either stdout or a file specified by the filename.
-// If filename is an empty string, it will write to stdout.
+// If the filename is an empty string, it writes to stdout.
 func WriteNotesTable(notes []Note, filename string) error {
 	var w io.Writer
+	// Determine the output destination based on the filename argument.
 	if filename == "" {
 		w = os.Stdout
 	} else {
+		// Ensure the filename has a .txt extension.
+		if !strings.HasSuffix(filename, ".txt") {
+			filename += ".txt"
+		}
+		// Create or truncate the file with the specified filename.
 		file, err := os.Create(filename)
 		if err != nil {
-			return err
+			return fmt.Errorf("failed to create file: %v", err)
 		}
-		defer file.Close()
+		defer file.Close() // Ensure the file is closed when the function exits.
 		w = file
 	}
 
+	// Write the header to the output destination.
 	header := "Selection\tView\tChannel\tBegin File\tBegin Time (s)\tEnd Time (s)\tLow Freq (Hz)\tHigh Freq (Hz)\tSpecies Code\tCommon Name\tConfidence\n"
 	if _, err := w.Write([]byte(header)); err != nil {
-		return err
+		return fmt.Errorf("failed to write header: %v", err)
 	}
 
+	// Iterate over the slice of notes, writing each to the output destination.
 	for i, note := range notes {
 		line := fmt.Sprintf("%d\tSpectrogram 1\t1\t%s\t%.1f\t%.1f\t0\t15000\t%s\t%s\t%.4f\n",
 			i+1, note.InputFile, note.BeginTime, note.EndTime, note.SpeciesCode, note.CommonName, note.Confidence)
 		if _, err := w.Write([]byte(line)); err != nil {
-			return err
+			return fmt.Errorf("failed to write note: %v", err)
 		}
 	}
 
+	// Return nil if the writing operation completes successfully.
 	return nil
 }
 
 // WriteNotesCsv writes the slice of notes to the specified destination in CSV format.
-// If filename is an empty string, it will write to stdout.
+// If filename is an empty string, the function writes to stdout.
+// The function returns an error if writing to the destination fails.
 func WriteNotesCsv(notes []Note, filename string) error {
+	// Define an io.Writer to abstract the writing operation.
 	var w io.Writer
+
+	// Determine the output destination based on the provided filename.
 	if filename == "" {
 		w = os.Stdout
 	} else {
+		// Ensure the filename has a .csv extension.
+		if !strings.HasSuffix(filename, ".csv") {
+			filename += ".csv"
+		}
+		// Create or truncate the file with the given filename.
 		file, err := os.Create(filename)
 		if err != nil {
-			return err
+			return fmt.Errorf("failed to create file %s: %w", filename, err)
 		}
 		defer file.Close()
 		w = file
 	}
 
+	// Define the CSV header.
 	header := "Start (s),End (s),Scientific name,Common name,Confidence\n"
+	// Write the header to the output destination.
 	if _, err := w.Write([]byte(header)); err != nil {
-		return err
+		return fmt.Errorf("failed to write header to CSV: %w", err)
 	}
 
+	// Iterate through the notes and write each one as a line in the CSV format.
 	for _, note := range notes {
 		line := fmt.Sprintf("%f,%f,%s,%s,%.4f\n",
 			note.BeginTime, note.EndTime, note.ScientificName, note.CommonName, note.Confidence)
 		if _, err := w.Write([]byte(line)); err != nil {
-			return err
+			return fmt.Errorf("failed to write note to CSV: %w", err)
 		}
 	}
 
+	// Return nil if the writing operation completes successfully.
 	return nil
 }
