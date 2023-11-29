75dab4eb1738e020e86f8e68165a46d116a9a3f9
Tomi P. Hakala <tomi.hakala@pobox.fi>
2023-11-29 21:01:14 +0200

    support for custom thresholds per species

--- internal/myaudio/process.go
+++ internal/myaudio/process.go
@@ -14,6 +14,156 @@ import (
 // processData processes the given audio data to detect bird species, logs the detected species
 // and optionally saves the audio clip if a bird species is detected above the configured threshold.
 func processData(data []byte, ctx *config.Context) error {
+	// temp assignment, fix later
+	const defaultBitDepth = 16
+
+	startTime := time.Now()
+
+	sampleData, err := ConvertToFloat32(data, defaultBitDepth)
+	if err != nil {
+		return fmt.Errorf("error converting to float32: %w", err)
+	}
+
+	results, err := predictSpecies(sampleData, ctx)
+	if err != nil {
+		return err
+	}
+
+	elapsedTime := logProcessingTime(startTime, ctx)
+
+	if err := processPredictionResults(results, data, ctx, elapsedTime); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func predictSpecies(sampleData [][]float32, ctx *config.Context) ([]birdnet.Result, error) {
+	results, err := birdnet.Predict(sampleData, ctx)
+	if err != nil {
+		return nil, fmt.Errorf("error predicting: %w", err)
+	}
+	return results, nil
+}
+
+func logProcessingTime(startTime time.Time, ctx *config.Context) time.Duration {
+	if ctx.Settings.ProcessingTime || ctx.Settings.Debug {
+		elapsedTime := time.Since(startTime)
+		fmt.Printf("\r\033[Kprocessing time %v ms", elapsedTime.Milliseconds())
+		return elapsedTime
+	}
+	return 0
+}
+
+func processPredictionResults(results []birdnet.Result, data []byte, ctx *config.Context, elapsedTime time.Duration) error {
+	if len(results) == 0 {
+		return nil
+	}
+
+	_, species, _ := observation.ParseSpeciesString(results[0].Species)
+	confidence := results[0].Confidence
+
+	// Use custom confidence threshold if it exists for the species, otherwise use the global threshold
+	confidenceThreshold, exists := ctx.CustomConfidence.Thresholds[species]
+	if !exists {
+		confidenceThreshold = float32(ctx.Settings.Threshold)
+	} else {
+		fmt.Printf("\nUsing confidence threshold of %.2f for %s", confidenceThreshold, species)
+	}
+
+	if confidence <= confidenceThreshold {
+		return nil
+	}
+
+	if !isSpeciesIncluded(results[0].Species, ctx.IncludedSpeciesList) {
+		if ctx.Settings.Debug {
+			fmt.Printf("\nSpecies not on included list: %s\n", species)
+		}
+		return nil
+	}
+
+	if ctx.OccurrenceMonitor.TrackSpecies(species) {
+		if ctx.Settings.Debug {
+			fmt.Printf("\nDuplicate occurrence detected: %s, skipping processing\n", species)
+		}
+		return nil
+	}
+
+	clipName := saveAudioClip(data, ctx)
+
+	return logObservation(ctx, results[0], clipName, elapsedTime)
+}
+
+func saveAudioClip(data []byte, ctx *config.Context) string {
+	if ctx.Settings.ClipPath == "" {
+		return ""
+	}
+
+	baseFileName := strconv.FormatInt(time.Now().Unix(), 10)
+	clipName := fmt.Sprintf("%s/%s.%s", ctx.Settings.ClipPath, baseFileName, ctx.Settings.ClipType)
+
+	var err error
+	switch ctx.Settings.ClipType {
+	case "wav":
+		err = savePCMDataToWAV(clipName, data)
+	case "flac":
+		err = savePCMDataToFlac(clipName, data)
+	case "mp3":
+		err = savePCMDataToMP3(clipName, data)
+	}
+
+	if err != nil {
+		fmt.Printf("error saving audio clip to %s: %s\n", ctx.Settings.ClipType, err)
+		return ""
+	} else if ctx.Settings.Debug {
+		fmt.Printf("Saved audio clip to %s\n", clipName)
+	}
+
+	return clipName
+}
+
+func logObservation(ctx *config.Context, result birdnet.Result, clipName string, elapsedTime time.Duration) error {
+	beginTime, endTime := 0.0, 0.0 // temporary assignments
+	note := observation.New(ctx.Settings, beginTime, endTime, result.Species, float64(result.Confidence), clipName, elapsedTime)
+
+	if err := observation.LogNote(ctx.Settings, note); err != nil {
+		fmt.Printf("error logging note: %s\n", err)
+		return err
+	}
+
+	fmt.Printf("%s %s %.2f\n", note.Time, note.CommonName, note.Confidence)
+	return nil
+}
+
+// isSpeciesIncluded checks if the given species is in the included species list.
+// It returns true if the species is in the list, or if the list is empty (no filtering).
+func isSpeciesIncluded(species string, includedList []string) bool {
+	if len(includedList) == 0 {
+		return true // no filtering applied when the list is empty
+	}
+	for _, s := range includedList {
+		if species == s {
+			return true
+		}
+	}
+
+	return false
+}
+
+// isSpeciesExcluded checks if the given species is in the excluded list.
+func isSpeciesExcluded(species string, excludedList []string) bool {
+	for _, excludedSpecies := range excludedList {
+		if species == excludedSpecies {
+			return true
+		}
+	}
+	return false
+}
+
+// processData processes the given audio data to detect bird species, logs the detected species
+// and optionally saves the audio clip if a bird species is detected above the configured threshold.
+/*
+func processData(data []byte, ctx *config.Context) error {
 
 	const defaultBitDepth = 16
 
@@ -142,7 +292,7 @@ func isSpeciesExcluded(species string, excludedList []string) bool {
 		}
 	}
 	return false
-}
+}*/
 
 // ConvertToFloat32 converts a byte slice representing sample to a 2D slice of float32 samples.
 // The function supports 16, 24, and 32 bit depths.
