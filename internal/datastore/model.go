7b7d8e0bfa165bbab97e2cb57f3d5dec81aeb6c4
Tomi Hakala <tomi.hakala@pobox.fi>
2024-02-25 17:23:03 +0200

    Refactor Note struct in model.go for improved indexing

--- internal/datastore/model.go
+++ internal/datastore/model.go
@@ -6,14 +6,14 @@ import "time"
 type Note struct {
 	ID             uint `gorm:"primaryKey"`
 	SourceNode     string
-	Date           string `gorm:"index:idx_notes_date_commonname_confidence,index:idx_notes_date_time"`
-	Time           string `gorm:"index:idx_notes_date_time"`
+	Date           string `gorm:"index:idx_notes_date;index:idx_notes_date_commonname_confidence"`
+	Time           string `gorm:"index:idx_notes_time"`
 	InputFile      string
 	BeginTime      time.Time
 	EndTime        time.Time
 	SpeciesCode    string
-	ScientificName string  `gorm:"index"`
-	CommonName     string  `gorm:"index;index:idx_notes_date_commonname_confidence"`
+	ScientificName string  `gorm:"index:idx_notes_sciname"`
+	CommonName     string  `gorm:"index:idx_notes_comname;index:idx_notes_date_commonname_confidence"`
 	Confidence     float64 `gorm:"index:idx_notes_date_commonname_confidence"`
 	Latitude       float64
 	Longitude      float64
@@ -22,9 +22,4 @@ type Note struct {
 	ClipName       string
 	Comment        string `gorm:"type:text"`
 	ProcessingTime time.Duration
-}
-
-type NoteWithSpectrogram struct {
-	Notes           []Note
-	SpectrogramPath string
 }
