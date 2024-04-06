b1dabfdd116256fde7904e9534236589265f3c43
Hampus Carlsson <carlsson889@gmail.com>
2024-04-06 18:06:39 +0200

    Add golangci-lint to devcontainer

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,10 @@
+# Install required apt dependencies
+apt-get update
+apt-get install -y ca-certificates libasound2 ffmpeg sox
+apt-get clean
+
+# Install air to support live reloading of server on code changes
+go install github.com/cosmtrek/air@latest
+
+# Install golangci-lint to allow running of linting locally
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.57.2
