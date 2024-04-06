1ed2e6858d6fb6916ce813a537cf18afad8923c9
Hampus Carlsson <carlsson889@gmail.com>
2024-04-06 16:28:04 +0000

    Add nano and vim to devcontainer

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,6 +3,7 @@
 # Install required apt dependencies
 apt-get update
 apt-get install -y ca-certificates libasound2 ffmpeg sox
+apt-get install -y nano vim
 apt-get clean
 
 # Install air to support live reloading of server on code changes
