aa80b9434406cd395d8e81768986213329761244
Tomi Hakala <tomi.hakala@pobox.fi>
2024-06-18 21:23:23 +0300

    refactor: improve thumbnails code, add cache size metrics for telemetry

--- internal/telemetry/metrics.go
+++ internal/telemetry/metrics.go
@@ -9,7 +9,8 @@ import (
 )
 
 type Metrics struct {
-	DetectionCounter *prometheus.CounterVec
+	DetectionCounter    *prometheus.CounterVec
+	ImageCacheSizeGauge prometheus.Gauge
 	// Additional metrics can be added here
 }
 
@@ -26,8 +27,17 @@ func NewMetrics() (*Metrics, error) {
 	}
 	labels := []string{"name"}
 	metrics.DetectionCounter = prometheus.NewCounterVec(counterOpts, labels)
-
 	if err := prometheus.Register(metrics.DetectionCounter); err != nil {
+		return nil, err
+	}
+
+	// Setup ImageCacheSizeGauge
+	gaugeOpts := prometheus.GaugeOpts{
+		Name: "birdnet_image_cache_size_bytes",
+		Help: "Current size of the bird image cache in bytes.",
+	}
+	metrics.ImageCacheSizeGauge = prometheus.NewGauge(gaugeOpts)
+	if err := prometheus.Register(metrics.ImageCacheSizeGauge); err != nil {
 		return nil, err
 	}
 
@@ -44,4 +54,9 @@ func RegisterMetricsHandlers(mux *http.ServeMux) {
 // IncrementDetectionCounter increments the detection counter for a given species
 func (m *Metrics) IncrementDetectionCounter(speciesName string) {
 	m.DetectionCounter.WithLabelValues(speciesName).Inc()
+}
+
+// SetImageCacheSize sets the current size of the image cache
+func (m *Metrics) SetImageCacheSize(size int) {
+	m.ImageCacheSizeGauge.Set(float64(size))
 }
