c412703d9ac2e897d615950c48a975b93cd026f7
Tomi Hakala <tomi.hakala@pobox.fi>
2024-06-23 01:09:06 +0300

    fix: layout improvements for smaller screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,14 @@ module.exports = {
     'lg:text-4xl',
   ],
   theme: {
-    extend: {},
+    screens: {
+      'xs': '400px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
