030e49d77415d5496ddd1b19b40a18abe72c3928
Tomi Hakala <tomi.hakala@pobox.fi>
2024-09-27 17:32:05 +0300

    enh: add buffersize const

--- internal/conf/consts.go
+++ internal/conf/consts.go
@@ -9,4 +9,7 @@ const (
 
 	SpeciesConfigCSV  = "species_config.csv"
 	SpeciesActionsCSV = "species_actions.csv"
+
+	// BufferSize is the size of the audio buffer in bytes, rounded up to the nearest 2048
+	BufferSize = ((SampleRate*NumChannels*CaptureLength*BitDepth/8 + 2047) / 2048) * 2048
 )
