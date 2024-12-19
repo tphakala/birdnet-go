1727e1557c5abb1d34bff12f3a857bef6b49b452
Tomi P. Hakala <tomi.hakala@pobox.fi>
2024-12-19 20:09:46 +0200

    feat: add CPU profiling support in main function
    - Implemented conditional CPU profiling based on the BIRDNET_GO_PROFILE environment variable.
    - Created a unique profile file with a timestamp for better traceability.
    - Updated error handling for configuration loading to improve robustness.

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"embed"
 	"fmt"
 	"os"
+	"runtime/pprof"
+	"time"
 
 	"github.com/spf13/viper"
 	"github.com/tphakala/birdnet-go/cmd"
@@ -21,18 +23,39 @@ var assetsFs embed.FS
 var viewsFs embed.FS
 
 func main() {
+	// Check if profiling is enabled
+	if os.Getenv("BIRDNET_GO_PROFILE") == "1" {
+		fmt.Println("Profiling enabled")
+		// Create a unique profile name with timestamp
+		now := time.Now()
+		profilePath := fmt.Sprintf("profile_%s.pprof", now.Format("20060102_150405"))
+
+		f, err := os.Create(profilePath)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error creating profile file: %v\n", err)
+			os.Exit(1)
+		}
+		defer f.Close()
+
+		if err := pprof.StartCPUProfile(f); err != nil {
+			fmt.Fprintf(os.Stderr, "Error starting CPU profile: %v\n", err)
+			os.Exit(1)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
 	// publish the embedded assets and views directories to controller package
 	httpcontroller.AssetsFs = assetsFs
 	httpcontroller.ViewsFs = viewsFs
 
 	// Load the configuration
-	settings, err := conf.Load()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error loading configuration: %v\n", err)
+	settings := conf.Setting()
+	if settings == nil {
+		fmt.Fprintf(os.Stderr, "Error loading configuration\n")
 		os.Exit(1)
-	} else {
-		fmt.Printf("BirdNET-Go build date: %s, using config file: %s\n", buildDate, viper.ConfigFileUsed())
 	}
+
+	fmt.Printf("🐦 \033[37mBirdNET-Go build date: %s, using config file: %s\033[0m\n", buildDate, viper.ConfigFileUsed())
 
 	// Execute the root command
 	rootCmd := cmd.RootCommand(settings)
